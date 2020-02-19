43a3707f218a33382f352e32759107e6ec3a64e2
Yoonho Kim <Daniel@MiMIX2-MiPhone.localdomain>
2020-02-19 11:57:21 -0800

    axios done, route for home/ videos done

--- sprint2/src/scripts/video.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function video(props) {
-  return (
-    <div className="video">
-      <video className="video__playing" poster={props.mainVideo.video}></video>
-    </div>
-  );
-}
