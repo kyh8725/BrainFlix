73828e70aaab855b9fe2f6b04719a3022c094e89
Yoonho Kim <Daniel@MiMIX2-MiPhone.localdomain>
2020-02-19 16:49:41 -0800

    router, upload page done

--- sprint2/src/App.js
+++ sprint2/src/App.js
@@ -6,6 +6,7 @@ import Video from "./components/video";
 import VideoInfo from "./components/videoInfo";
 import VideoList from "./components/videoList";
 import VideoControls from "./components/videoControls";
+import Upload from "./components/upload";
 import axios from "axios";
 import "./styles/main.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -25,17 +26,16 @@ class App extends Component {
   componentDidMount() {
     axios.get($url + $key).then(response => {
       this.setState({ videoList: response.data });
-    });
-    axios.get(`${$url}${this.state.mainVideoId}${$key}`).then(response => {
-      const mainVideo = response.data;
-      this.setState({ mainVideo });
-      this.setState({ commentCount: mainVideo.comments.length });
-      this.setState({ comments: mainVideo.comments });
+      axios.get(`${$url}${$homeKey}${$key}`).then(response => {
+        const mainVideo = response.data;
+        this.setState({ mainVideo });
+        this.setState({ commentCount: mainVideo.comments.length });
+        this.setState({ comments: mainVideo.comments });
+      });
     });
   }
 
   componentDidUpdate(prevProp, prevState) {
-    console.log("in did update ", prevState.mainVideoId);
     if (prevState.mainVideoId !== this.state.mainVideoId) {
       axios.get(`${$url}${this.state.mainVideoId}${$key}`).then(response => {
         const mainVideo = response.data;
@@ -52,9 +52,21 @@ class App extends Component {
         <Header />
         <Switch>
           <Route
-            path="/"
-            exact
+            path="/upload"
             render={() => {
+              return (
+                <main>
+                  <Upload />
+                </main>
+              );
+            }}
+          />
+          <Route
+            path="/:id"
+            render={props => {
+              if (props.match.params.id !== this.state.mainVideoId) {
+                this.setState({ mainVideoId: props.match.params.id });
+              }
               return (
                 <main>
                   <div className="mainVideo-container">
@@ -78,13 +90,12 @@ class App extends Component {
               );
             }}
           />
-
           <Route
-            path="/:id"
-            render={props => {
-              if (props.match.params.id !== this.state.mainVideoId) {
-                this.setState({ mainVideoId: props.match.params.id });
-                //console.log(props.match.params.id);
+            path="/"
+            exact
+            render={() => {
+              if ($homeKey !== this.state.mainVideoId) {
+                this.setState({ mainVideoId: $homeKey });
               }
               return (
                 <main>
@@ -114,83 +125,4 @@ class App extends Component {
     );
   }
 }
-
 export default App;
-
-/**
- * Functional Requirements
-Clicking the BrainFlix Logo must link back to the home page.
-There must be 3 routes:
-The Home/Video Player Page for displaying the details of a video.
-The Video Upload Page.
-A route which will load the video with the provided video id to be displayed using the Video Player Page.
-Clicking on a video in the “Next Video” list must go to the Video Details Page for the selected video and display all the information for the video such as (likes, views, author, etc).
-Clicking on the “Upload” button must link to the Video Upload Page.
-
-
-const Home = () => <h1>Home</h1>;
-const Images = props => {
-  const images = props.images.map(image => (
-    <Link to={`images/${image.id}`} key={image.id}>
-      <img src={image.url} alt={image.title} />
-    </Link>
-  ));
-  return images;
-};
-const ImageDetails = props => (
-  <div>
-    <img src={props.url} alt={props.title} />
-    <h2>{props.title}</h2>
-  </div>
-);
-
-class App extends Component {
-  state = {
-    images: [
-      {
-        id: "1",
-        title: "Image 1",
-        url: "https://i.picsum.photos/id/237/200/300.jpg"
-      },
-      {
-        id: "2",
-        title: "Image 2",
-        url: "https://i.picsum.photos/id/238/200/300.jpg"
-      },
-      {
-        id: "3",
-        title: "Image 3",
-        url: "https://i.picsum.photos/id/239/200/300.jpg"
-      }
-    ]
-  };
-  render() {
-    return (
-      <Router>
-        <Link to="/images">Images</Link>
-        <Switch>
-          <Route path="/" component={Home} exact />
-          <Route
-            path="/images"
-            exact
-            render={() => <Images images={this.state.images} />}
-          />
-          <Route
-            path="/images/:id"
-            render={props => {
-              const image = this.state.images.find(image => {
-                return props.match.params.id === image.id;
-              });
-              return <ImageDetails title={image.title} url={image.url} />;
-            }}
-          />
-        </Switch>
-      </Router>
-    );
-  }
-}
-
-export default App;
-
-
- */
---
+++ sprint2/src/components/upload.js
@@ -0,0 +1,42 @@
+import React from "react";
+import uploadImage from "../assets/Images/Upload-video-preview.jpg";
+
+export default function upload() {
+  return (
+    <div className="upload">
+      <div className="upload__header">
+        <h1>Upload Video</h1>
+      </div>
+      <div className="upload__contents">
+        <div className="upload__thumbnail">
+          <p className="upload__thumbnail-title">VIDEO THUMBNAIL</p>
+          <div className="upload__thumbnail-image">
+            <img src={uploadImage} />
+          </div>
+        </div>
+        <div className="upload__video">
+          <form>
+            <p className="upload__video-title">TITLE YOUR VIDEO</p>
+            <input
+              className="upload__video-title-input"
+              name="video-input"
+              type="/text"
+              placeholder="Add a title to your video"
+            ></input>
+            <p className="upload__video-desc-title">ADD A VIDEO DESCRIPTION</p>
+            <textarea
+              className="upload__video-desc-input"
+              name="video-input"
+              type="/text"
+              placeholder="Add a description of your video"
+            ></textarea>
+          </form>
+        </div>
+      </div>
+      <div className="upload__buttons">
+        <button className="upload__buttons-publish">PUBLISH</button>
+        <button className="upload__buttons-cancel">CANCEL</button>
+      </div>
+    </div>
+  );
+}
